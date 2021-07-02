affbfdc25760920502b59350f7f24315e5fd189c
Guillaume Roux <rouxguillaume8@gmail.com>
2021-07-02 17:54:05 +0200

    removed staggered grid view

--- lib/ui/projects_view/mobile/projects_mobile.dart
+++ lib/ui/projects_view/mobile/projects_mobile.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:my_portfolio/bloc/bloc_provider.dart';
 import 'package:my_portfolio/bloc/projects_bloc.dart';
 import 'package:my_portfolio/ui/projects_view/common/project_widget.dart';
@@ -9,18 +8,15 @@ class ProjectsMobile extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     final projectsBloc = BlocProvider.of<ProjectsBloc>(context);
-    return StaggeredGridView.countBuilder(
+    return ListView.separated(
       padding: EdgeInsets.symmetric(
         vertical: size.height * 0.05,
         horizontal: size.width * 0.05,
       ),
       shrinkWrap: true,
-      crossAxisCount: 2,
-      mainAxisSpacing: size.width * 0.05,
-      crossAxisSpacing: size.width * 0.05,
       itemCount: projectsBloc.projects.length,
       itemBuilder: (_, index) => ProjectWidget(projectsBloc.projects[index]),
-      staggeredTileBuilder: (_) => const StaggeredTile.fit(1),
+      separatorBuilder: (_, __) => SizedBox(height: 16),
     );
   }
 }
