132bcc9fe80020ba580b59bb9962ec6b1e5b1114
Guillaume Roux <rouxguillaume8@gmail.com>
2022-03-14 15:47:50 +0100

    fixed tab bar

--- lib/ui/common/custom_tab_bar.dart
+++ lib/ui/common/custom_tab_bar.dart
@@ -3,24 +3,18 @@ import 'package:flutter/material.dart';
 import '../../bloc/bloc_provider.dart';
 import '../../bloc/navigation_bloc.dart';
 import 'custom_tab.dart';
-import 'responsive_layout.dart';
 
 class CustomTabBar extends StatelessWidget {
   final TabController controller;
 
-  const CustomTabBar({
-    Key? key,
-    required this.controller,
-  }) : super(key: key);
+  const CustomTabBar({Key? key, required this.controller}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     final currentTheme = Theme.of(context);
-    final tabScaling = ResponsiveLayout.isDesktop(context) ? 0.21 : 0.4;
-    return Container(
-      margin: EdgeInsets.only(right: size.width * 0.05),
-      width: size.width * tabScaling,
+    return Padding(
+      padding: EdgeInsets.only(right: size.width * 0.05),
       child: Theme(
         data: currentTheme.copyWith(
           highlightColor: Colors.transparent,
@@ -28,6 +22,7 @@ class CustomTabBar extends StatelessWidget {
           hoverColor: Colors.transparent,
         ),
         child: TabBar(
+          isScrollable: true,
           indicatorSize: TabBarIndicatorSize.tab,
           controller: controller,
           tabs: BlocProvider.of<NavigationBloc>(context)
