3b2ec7c9dfa689a2c55f0d459e6c4ab59179eae9
Guillaume Roux <rouxguillaume8@gmail.com>
2023-01-08 02:49:55 +0100

    new grid design almost ready

--- lib/ui/articles_view/desktop/articles_desktop.dart
+++ lib/ui/articles_view/desktop/articles_desktop.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:my_portfolio/ui/articles_view/controller/articles_controller.dart';
 import 'package:my_portfolio/ui/articles_view/desktop/widgets/article_case.dart';
 
@@ -20,12 +21,16 @@ class ArticlesDesktop extends ConsumerWidget {
         final crossAxisCount = (size.width / caseWidth).floor();
         final articles = loaded.articles;
 
-        return GridView.count(
-          padding: const EdgeInsets.all(32),
+        return MasonryGridView.count(
+          // padding: const EdgeInsets.all(32),
           mainAxisSpacing: _kSpacing,
           crossAxisSpacing: _kSpacing,
           crossAxisCount: crossAxisCount,
-          children: [...articles.map((e) => ArticleCase(e, width: caseWidth))],
+          itemCount: articles.length,
+          itemBuilder: (context, index) {
+            final article = articles[index];
+            return ArticleCase(article, width: caseWidth);
+          },
         );
       },
       error: (error) => Center(child: Text(error.message)),
