ee4773841fa3b28f0a4c62a6a68997a215f73274
Guillaume Roux <rouxguillaume8@gmail.com>
2023-01-08 19:04:09 +0100

    simplified code

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_portfolio/core/extensions/iterable_extensions.dart';
 import 'package:my_portfolio/core/router.dart';
 import 'package:my_portfolio/features/localization/providers/app_localizations_providers.dart';
 import 'package:my_portfolio/l10n/app_localizations.dart';
@@ -17,6 +18,7 @@ class MyApp extends ConsumerWidget {
     return MaterialApp.router(
       title: 'Guillaume Roux - Flutter Developer',
       theme: MyThemes.dark,
+      darkTheme: MyThemes.dark,
       routerDelegate: router.routerDelegate,
       routeInformationParser: router.routeInformationParser,
       routeInformationProvider: router.routeInformationProvider,
@@ -29,10 +31,7 @@ class MyApp extends ConsumerWidget {
       locale: locale,
       supportedLocales: supportedLocales,
       localeResolutionCallback: (locale, supportedLocales) {
-        return supportedLocales.firstWhere(
-          (l) => l.languageCode == locale?.languageCode,
-          orElse: () => supportedLocales.first,
-        );
+        return supportedLocales.firstMatch(locale);
       },
     );
   }
